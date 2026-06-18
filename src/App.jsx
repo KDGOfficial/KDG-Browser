282d3d0a5d479779290632496786e796e37e5293
KDGOfficial <delovoykdg@gmail.com>
2026-06-18 14:57:42 +0300

    Bump version to 3.4.20

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { Settings }       from './pages/Settings';
 import { UpdateOverlay }  from './components/UpdateOverlay';
 import { MigrationWizardOverlay } from './components/MigrationWizardOverlay';
 
-const BROWSER_VERSION = '3.4.19';
+const BROWSER_VERSION = '3.4.20';
 
 export default function App() {
   const electronAPI = window.electronAPI;
