0fd3c9e7490c9772a4a36d12549e3eb6ebeb24e2
KDGOfficial <delovoykdg@gmail.com>
2026-06-21 01:02:27 +0300

    chore(release): bump version to 3.7.6

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { Extensions }     from './pages/Extensions';
 import { UpdateOverlay }  from './components/UpdateOverlay';
 import { MigrationWizardOverlay } from './components/MigrationWizardOverlay';
 
-const BROWSER_VERSION = '3.6.6';
+const BROWSER_VERSION = '3.7.6';
 
 export default function App() {
   const electronAPI = window.electronAPI;
