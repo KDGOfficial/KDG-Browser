f4fdb66098537429130a1fb2cd8ad17fd1f87225
KDGOfficial <delovoykdg@gmail.com>
2026-06-20 23:38:33 +0300

    ci: use native publish:win step in release workflow and bump version to 3.6.6

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { Extensions }     from './pages/Extensions';
 import { UpdateOverlay }  from './components/UpdateOverlay';
 import { MigrationWizardOverlay } from './components/MigrationWizardOverlay';
 
-const BROWSER_VERSION = '3.6.5';
+const BROWSER_VERSION = '3.6.6';
 
 export default function App() {
   const electronAPI = window.electronAPI;
