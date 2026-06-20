ba89906da83a58667d97a89040cd0101129c66fa
KDGOfficial <delovoykdg@gmail.com>
2026-06-20 23:28:37 +0300

    ci: add MSVC compilation step to release workflow and bump version to 3.6.5

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { Extensions }     from './pages/Extensions';
 import { UpdateOverlay }  from './components/UpdateOverlay';
 import { MigrationWizardOverlay } from './components/MigrationWizardOverlay';
 
-const BROWSER_VERSION = '3.6.4';
+const BROWSER_VERSION = '3.6.5';
 
 export default function App() {
   const electronAPI = window.electronAPI;
